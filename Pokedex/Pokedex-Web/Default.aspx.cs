db0ffa08a326b6ccded9571ceb25c714856cb252
Jesus Moreno <ing.jmoreno94@gmail.com>
2024-04-08 07:13:05 -0500

    Se agrega a la ventana default una presentacion en tarjeta de los pokemons, una usando forEach y otra usando un Repeater

--- Pokedex/Pokedex-Web/Default.aspx.cs
+++ Pokedex/Pokedex-Web/Default.aspx.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio;
+using negocio;
 
 namespace Pokedex_Web
 {
     public partial class PokedexWeb : System.Web.UI.Page
     {
+        public List<Pokemon> listPokemon { get; set; }        
         protected void Page_Load(object sender, EventArgs e)
         {
+            PokemonNegocio negocio = new PokemonNegocio();
+            listPokemon = negocio.listarConSP();
+
+            if(!IsPostBack) 
+            {
+                reRepetidor.DataSource = listPokemon;
+                reRepetidor.DataBind();
+            }
+        }
+
+        protected void btnEjemplo_Click(object sender, EventArgs e)
+        {
+            string var = ((Button)sender).CommandArgument;
 
         }
     }
