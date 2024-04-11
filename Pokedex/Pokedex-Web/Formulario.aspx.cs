1628b22c516e370d2690dcfbe36564df027df8e2
Jesus Moreno <ing.jmoreno94@gmail.com>
2024-04-11 06:19:01 -0500

    Se agrega la logica al boton agregar. ya se puede agregar elementos a la BBDD

--- Pokedex/Pokedex-Web/Formulario.aspx.cs
+++ Pokedex/Pokedex-Web/Formulario.aspx.cs
@@ -15,28 +15,61 @@ namespace Pokedex_Web
         {
             txtId.Enabled = false;
 
-            if (!IsPostBack)
+            try
             {
-                ElementoNegocio negocio = new ElementoNegocio();
-                List<Elemento> lista = negocio.listar();
+                if (!IsPostBack)
+                {
+                    ElementoNegocio negocio = new ElementoNegocio();
+                    List<Elemento> lista = negocio.listar();
 
-                ddlTipo.DataSource = lista;
-                ddlTipo.DataValueField = "Id";
-                ddlTipo.DataTextField = "Descripcion";
-                ddlTipo.DataBind();
+                    ddlTipo.DataSource = lista;
+                    ddlTipo.DataValueField = "Id";
+                    ddlTipo.DataTextField = "Descripcion";
+                    ddlTipo.DataBind();
 
-                ddlDebilidad.DataSource = lista;
-                ddlDebilidad.DataValueField = "Id";
-                ddlDebilidad.DataTextField = "Descripcion";
-                ddlDebilidad.DataBind();
+                    ddlDebilidad.DataSource = lista;
+                    ddlDebilidad.DataValueField = "Id";
+                    ddlDebilidad.DataTextField = "Descripcion";
+                    ddlDebilidad.DataBind();
 
+                }
             }
+            catch (Exception ex)
+            {
+
+                Session.Add("error", ex);
+                throw;
+                //redireccion a pantalla de error
+            }
+
 
         }
 
         protected void btnAceptar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Pokemon aux = new Pokemon();
+                PokemonNegocio negocio = new PokemonNegocio();
+                aux.Numero = int.Parse(txtNumero.Text);
+                aux.Nombre = txtNombre.Text;
+                aux.Descripcion = txtDescripcion.Text;
+                aux.UrlImagen = txtUrlImagen.Text;
+                aux.Tipo = new Elemento();
+                aux.Tipo.Id = int.Parse(ddlTipo.SelectedValue);
+                aux.Debilidad = new Elemento();
+                aux.Debilidad.Id = int.Parse(ddlDebilidad.SelectedValue);
 
+                negocio.agregarPokemon(aux);
+                Response.Redirect("ListaPokemon.aspx", false);
+            }
+            catch (Exception ex)
+            {
+
+                Session.Add("error", ex);
+                throw;
+                //redireccion a pantalla de error
+            }
         }
 
         protected void txtUrlImagen_TextChanged(object sender, EventArgs e)
